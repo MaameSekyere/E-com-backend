7ee66d5f84d25f72022aa2514ab33fedd8a9373a
MaameSekyere <maameant@gmail.com>
2022-08-03 14:50:03 -0400

    added tag routes

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,75 @@
+const router = require("express").Router();
+const { Tag, ProductTag, Product } = require("../../models");
+
+router.get("/", (req, res) => {
+  Tag.findAll({ include: [{ model: Product }] })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Tag.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No Tag found with this id!" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.put("/", (req, res) => {
+  Tag.update({ tag_name: req.body.tag_name }, { where: { id: req.params.id } })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tags found" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag found with this id!" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
