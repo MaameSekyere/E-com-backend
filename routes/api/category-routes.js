34c497c76151033fcf7be062a91c96bb63bd5bfb
MaameSekyere <maameant@gmail.com>
2022-08-03 13:56:24 -0400

    Update category-routes.js

---
+++ routes/api/category-routes.js
@@ -0,0 +1,83 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+//find all categories
+router.get("/", (req, res) => {
+  Category.findAll({ include: [{ model: Product }] })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//find a category
+router.get("/:id", (req, res) => {
+  Category.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  })
+    .then((dbcategoryData) => {
+      if (!dbcategoryData) {
+        res.status(404).json({ message: "No categories found" });
+        return;
+      }
+      res.json(dbcategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//create a category
+router.post("/", (req, res) => {
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+//update a category
+router.put("/", (req, res) => {
+  Category.update(
+    { category_name: req.body.category_name },
+    { where: { id: req.params.id } }
+  )
+    .then((dbcategoryData) => {
+      if (!dbcategoryData) {
+        res.status(404).json({ message: "No categories found" });
+        return;
+      }
+      res.json(dbcategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//delete a category
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No category found with this id!" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
