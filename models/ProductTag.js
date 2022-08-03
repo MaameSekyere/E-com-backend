20afef3167318e86dba8b63312305ce69ec823da
MaameSekyere <maameant@gmail.com>
2022-08-03 00:10:54 -0400

    added all model files

---
+++ models/ProductTag.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+//const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+// create our ProductTag model
+class ProductTag extends Model {}
+
+// create fields/columns for User model
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product_tag",
+  }
+);
+
+module.exports = ProductTag;
---
+++ models/Tag.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "tag",
+  }
+);
+
+module.exports = Tag;
